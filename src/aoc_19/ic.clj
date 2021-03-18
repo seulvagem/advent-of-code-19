dc59ea66e8e15652a4a5c6d40c95dc5a1c7a2dca
Selvagem <ffelipe.dias@gmail.com>
2021-03-18 16:04:22 -0300

    some changes to intcode computer (WIP)

--- src/aoc_19/ic.clj
+++ src/aoc_19/ic.clj
@@ -4,5 +4,14 @@
 
 ;;intcode computer
 
-(s/def ::intcode (s/+ integer?))
+(s/def ::intcode (s/and vector? (s/+ integer?)))
 
+(defn run
+  "takes an intcode and runs from the start"
+  [intcode]
+  {:pre [(s/valid? ::intcode intcode)]}
+  )
+
+(s/fdef run
+  :args #(s/valid? ::intcode (first %))
+  :ret #(s/valid? ::intcode %))
