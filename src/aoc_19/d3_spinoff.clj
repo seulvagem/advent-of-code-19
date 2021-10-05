6bb29fee74602da88f0a96c466d39ba7c7ef5899
Selvagem <ffelipe.dias@gmail.com>
2021-10-05 12:03:47 -0300

    did most of the specs for the part 1 version of d3 spinoff, PRETTY COOL NGL

--- src/aoc_19/d3_spinoff.clj
+++ src/aoc_19/d3_spinoff.clj
@@ -1,9 +1,13 @@
 (ns aoc-19.d3-spinoff
   (:gen-class)
   (:require [clojure.spec.alpha :as s]
-            [clojure.spec.gen.alpha :as gen]
+            [clojure.spec.test.alpha :as stest]
             [aoc-19.base :as b]
             [aoc-19.d3 :as d3]))
+
+;; specs
+
+(s/def ::coord-key #{:x :y})
 
 (s/def ::coordinate int?)
 (s/def ::coordinate-range (s/tuple int? int?))
@@ -14,8 +18,19 @@
 (s/def ::x-range ::coordinate-range)
 (s/def ::y-range ::coordinate-range)
 
+(s/def ::pos (s/keys :req-un [::x ::y]))
 (s/def ::h-line (s/keys :req-un [::x-range ::y]))
 (s/def ::v-line (s/keys :req-un [::x ::y-range]))
+(s/def ::line (s/or :h-line ::h-line
+                    :v-line ::v-line))
+
+(s/def ::h-lines (s/every ::h-line))
+(s/def ::v-lines (s/every ::v-line))
+
+(s/def ::board (s/keys :req-un [::h-lines ::v-lines]))
+
+;; specs 
+
 
 (defn h?
   [line]
@@ -33,31 +48,43 @@
   {:x x
    :y-range [y1 y2]})
 
-(defn get-intersection
-  "takes a h-line and a v-line, gets the intersection point if they have one"
-  [{[hxi hxf] :x-range, hy :y} {vx :x, [vyi vyf] :y-range}]
-  (when (and (<= hxi vx hxf)
-             (<= vyi hy vyf))
-    [vx hy]))
+
+(s/fdef ->line
+  :args (s/cat :fix-key ::coord-key
+               :range-key ::coord-key
+               :initial-position ::pos
+               :oriented-distance int?)
+  :ret (s/tuple ::line ::pos))
 
 (defn ->line
   [fix-k range-k i-pos dist]
   (let [range-key (-> range-k name (str "-range") keyword)
         range-i-val (range-k i-pos)
         range-f-val (+ dist range-i-val)
-        range-tuple (sort [range-i-val range-f-val])
+        range-tuple (vec (sort [range-i-val range-f-val]))
         end-pos (assoc i-pos range-k range-f-val)]
     [{fix-k (fix-k i-pos)
       range-key range-tuple}
      end-pos]))
 
 (def dir->line-config
-  {"R" [:h-line #(->line :y :x %1 %2)]
-   "L" [:h-line #(->line :y :x %1 (- %2))]
-   "U" [:v-line #(->line :x :y %1 %2)]
-   "D" [:v-line #(->line :x :y %1 (- %2))]})
+  {"R" [:h-lines #(->line :y :x %1 %2)]
+   "L" [:h-lines #(->line :y :x %1 (- %2))]
+   "U" [:v-lines #(->line :x :y %1 %2)]
+   "D" [:v-lines #(->line :x :y %1 (- %2))]})
 
-(defn acc-lines-to-board 
+
+(def line-configs
+  (set (vals dir->line-config)))
+
+(s/fdef acc-lines-to-board ;; doesn't generate yet, probably need to use the ->line spec or something
+  :args (s/alt :step-arity (s/cat :board ::board
+                                  :step-input (s/tuple line-configs int?))
+               :completing-arity (s/cat :board ::board)
+               :initial-item-arity (s/cat))
+  :ret ::board)
+
+(defn acc-lines-to-board
   ([{pos :pos :as board} [[line-type ->line] dist]]
    (let [[nline npos] (->line pos dist)]
      (-> board
@@ -68,8 +95,26 @@
   ([]
    {:pos {:x 0
           :y 0}
-    :h-line []
-    :v-line []}))
+    :h-lines []
+    :v-lines []}))
+
+(s/fdef get-intersection
+  :args (s/cat :h-line ::h-line
+               :v-line ::v-line)
+  :ret (s/alt :intersection :aoc-19.d3/loc
+              :no-intersection nil?))
+
+(defn get-intersection
+  "takes a h-line and a v-line, gets the intersection point if they have one"
+  [{[hxi hxf] :x-range, hy :y} {vx :x, [vyi vyf] :y-range}]
+  (when (and (<= hxi vx hxf)
+             (<= vyi hy vyf))
+    [vx hy]))
+
+(s/fdef get-line-intersections
+  :args (s/cat :v-lines ::v-lines
+               :h-line ::h-line)
+  :ret (s/* ::pos))
 
 (defn get-line-intersections
   [v-lines h-line]
@@ -77,13 +122,23 @@
                  (filter identity))]
     (sequence xf v-lines)))
 
+(s/fdef intersecs
+  :args (s/cat :h-lines ::h-lines
+               :v-lines ::v-lines)
+  :ret (s/* ::pos))
+
 (defn intersecs
   [h-lines v-lines]
   (mapcat #(get-line-intersections v-lines %) h-lines))
 
 
+(s/fdef get-boards-intersections
+  :args (s/cat :board-1 ::board
+               :board-2 ::board)
+  :ret (s/* ::pos))
+
 (defn get-boards-intersections
-  [{b1-hs :h-line, b1-vs :v-line} {b2-hs :h-line, b2-vs :v-line}]
+  [{b1-hs :h-lines, b1-vs :v-lines} {b2-hs :h-lines, b2-vs :v-lines}]
   (concat (intersecs b1-hs b2-vs)
           (intersecs b2-hs b1-vs)))
 
@@ -93,14 +148,14 @@
         x-instr->line-config (comp d3/x-instr->moves
                                    (map d3/parse-move-string)
                                    (map (fn [[dir step]]
-                                          (list (dir->line-config dir) (b/parse-int step)))))
+                                          (vector (dir->line-config dir) (b/parse-int step)))))
         boards (mapv #(transduce x-instr->line-config acc-lines-to-board %) instr-lists)
         intersections (apply get-boards-intersections boards)
-        res1 (d3/get-closest-loc intersections)
-        ]
-    [res1])) 
+        res1 (d3/get-closest-loc-by d3/x-loc-center-dist intersections)]
+    [res1 (first boards)]))
 
 (defn -main
   []
   (get-results (b/get-input 3)))
 
+(stest/instrument)
